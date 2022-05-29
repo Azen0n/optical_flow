d0a2f9b3f4894295d1a3f04faa035b1b04a694b0
Azen0n <jack.2013.ghost@gmail.com>
2022-05-29 23:58:36 +0500

    Add Horn-Schunck method implementation

---
+++ horn_schunck.py
@@ -0,0 +1,77 @@
+import numpy as np
+from scipy.ndimage import convolve
+import cv2
+from matplotlib import pyplot as plt
+
+
+def horn_schunck(first_image: np.ndarray, second_image: np.ndarray,
+                 alpha: float = 1.0, number_of_iterations: int = 10) -> tuple:
+    """Horn–Schunck method of estimating optical flow.
+
+    first_image, second_image — two frames for optical flow estimation.
+    alpha — regularization constant. Larger values lead to a smoother flow.
+    number_of_iterations — number of iterations for laplacian approximation.
+    returns flow vectors U, V."""
+    first_image = cv2.cvtColor(first_image, cv2.COLOR_BGR2GRAY)
+    second_image = cv2.cvtColor(second_image, cv2.COLOR_BGR2GRAY)
+
+    u = np.zeros(first_image.shape)
+    v = np.zeros(first_image.shape)
+
+    dx, dy, dt = calc_image_derivatives(first_image, second_image)
+
+    averaging_kernel = np.array([[1 / 12, 1 / 6, 1 / 12],
+                                 [1 / 6, 0, 1 / 6],
+                                 [1 / 12, 1 / 6, 1 / 12]])
+
+    for _ in range(number_of_iterations):
+        u_averages = convolve(u, averaging_kernel)
+        v_averages = convolve(v, averaging_kernel)
+
+        derivative = (dx * u_averages + dy * v_averages + dt) / (alpha ** 2 + dx ** 2 + dy ** 2)
+        u = u_averages - dx * derivative
+        v = v_averages - dy * derivative
+
+    return u, v
+
+
+def calc_image_derivatives(first_image: np.ndarray, second_image: np.ndarray) -> tuple:
+    """Calculate image derivatives along the x, y and time dimensions."""
+    kernel_x = np.array([[-1, 1],
+                         [-1, 1]]) * 0.25
+    kernel_y = np.array([[-1, -1],
+                         [1, 1]]) * 0.25
+    kernel_t = np.array([[1, 1],
+                         [1, 1]]) * 0.25
+
+    dx = convolve(first_image, kernel_x) + convolve(second_image, kernel_x)
+    dy = convolve(first_image, kernel_y) + convolve(second_image, kernel_y)
+    dt = convolve(first_image, kernel_t) + convolve(second_image, -kernel_t)
+
+    return dx, dy, dt
+
+
+def single():
+    first_image = cv2.imread('first.jpg')
+    second_image = cv2.imread('second.jpg')
+    u, v = horn_schunck(first_image, second_image, 90, 2)
+    plt.imshow(u + v, cmap='gray')
+    plt.show()
+
+
+def main():
+    alpha = 20
+    number_of_iterations = 2
+    capture = cv2.VideoCapture('video.mp4')
+    while True:
+        _, first_frame = capture.read()
+        _, second_frame = capture.read()
+        u, v = horn_schunck(first_frame, second_frame, alpha, number_of_iterations)
+        cv2.imshow(f'Horn-Schunck method, {alpha = }, it = {number_of_iterations}', u + v)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+    main()
